abe85f426bfb7cfd830e803abb7e6508c2a57ad6
Redinger <tracy.redinger@ourlotto.com>
2026-01-27 11:52:32 -0500

    Fix build: rely on public/ for staticwebapp.config.json

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import fs from "fs";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    {
-      name: "copy-swa-config",
-      closeBundle() {
-        fs.copyFileSync(
-          resolve(__dirname, "staticwebapp.config.json"),
-          resolve(__dirname, "dist/staticwebapp.config.json")
-        );
-      },
-    },
-  ],
+  plugins: [react()],
 });
